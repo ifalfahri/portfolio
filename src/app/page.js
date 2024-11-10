0b2986bf357da2f6b42a2421dd08a1e9ad873a66
IfalEXZ <ifaltampan69@gmail.com>
2024-11-10 10:00:30 +0800

    fix: setup preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,35 @@
 'use client';
-import { useEffect } from 'react'
+import styles from './page.module.scss'
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader';
+import Landing from '../components/Landing';
+import Projects from '../components/Projects';
+import Description from '../components/Description';
+import SlidingImages from '../components/SlidingImages';
+import Contact from '../components/Contact';
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect( () => {
     (
       async () => {
           const LocomotiveScroll = (await import('locomotive-scroll')).default
           const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
       }
     )()
   }, [])
 
   return (
-    <main>
+    <main className={styles.main}>
       <AnimatePresence mode='wait'>
         {isLoading && <Preloader />}
       </AnimatePresence>
